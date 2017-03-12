cca1473e64a02f1d834f236d60b5bd1b141a7a83
esneyder <edesalla17@gmail.com>
2017-03-11 20:54:06 -0500

    file

---
+++ Contacts/Models/ContactView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Contacts.Models
+{
+    [NotMapped]
+    public class ContactView : Contact
+    {
+        [Display(Name = "Image")]
+        public HttpPostedFileBase ImageFile { get; set; }
+    }
+
+}
---
+++ Contacts/Models/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Contacts.Models
+{
+    public class DataContext: DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Contacts.Models.Contact> Contacts { get; set; }
+    }
+}
